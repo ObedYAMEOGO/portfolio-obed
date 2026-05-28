98cfa93af9385e95c308b7b6439d0b9e47eef9a3
Obed/Y <109439216+ObedYAMEOGO@users.noreply.github.com>
2026-05-29 03:12:37 +0530

    updated ProjectCard.tsx and ContactModule

--- frontend/src/components/ProjectCard.tsx
+++ frontend/src/components/ProjectCard.tsx
@@ -402,7 +402,7 @@ export default function ProjectCard({ project, index = 0 }: ProjectCardProps) {
                     >
                       {isTechExpanded
                         ? "Show less"
-                        : `+${project.tech_stack.length - 4} more`}
+                        : `+${project.tech_stack.length - 6} more`}
                     </button>
                   )}
                 </div>
